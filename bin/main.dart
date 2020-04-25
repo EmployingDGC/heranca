d6f124b37dfa6213c73f581b1bbf1e1a401ee7a9
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-04-25 18:09:50 -0300

    Update main.dart
    Implementada as classes semelhantes para aplicar a herança

--- bin/main.dart
+++ bin/main.dart
@@ -4,9 +4,35 @@ void main(List<String> arguments) {
 
 
 class Cachorro {
-  
+  String nome;
+  double peso;
+  int fofura;
+
+  void comer() {
+    print('${this.nome} comeu');
+  }
+
+  void latir() {
+    print('Au! Au!');
+  }
+
+  void brincar() {
+    fofura += 10;
+    print('Fofura do ${this.nome} aumentou para ${this.fofura}!!!');
+  }
 }
 
 class Gato {
-  
+  String nome;
+  double peso;
+
+  void comer() {
+    print('${this.nome} comeu');
+  }
+
+  void miar() {
+    print('Miau! Miau!');
+  }
+
+  bool estaAmigavel() => true;
 }
